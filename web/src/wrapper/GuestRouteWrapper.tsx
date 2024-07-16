a890f144a1bccb8f596a032ad701477abe5fb688
hubirto <hubert.rybicki.hr@gmail.com>
2024-07-16 18:39:55 +0200

    Add basic auth in frontend

---
+++ web/src/wrapper/GuestRouteWrapper.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {useAuth} from "@/providers/AuthContext.tsx";
+import {Navigate, useLocation} from "react-router-dom";
+import {Spinner} from "@/components/Spinner.tsx";
+
+const GuestRouteWrapper = ({children}: { children: React.ReactNode }) => {
+    const location = useLocation();
+    const {authState, isLoading} = useAuth();
+
+    if (isLoading) {
+        return (
+            <div className="flex items-center justify-center min-h-screen">
+                <Spinner/>
+            </div>
+        );
+    }
+
+    if (authState?.token) {
+        return <Navigate to="/" state={{from: location}}/>
+    }
+
+    return <>{children}</>
+};
+
+export default GuestRouteWrapper;
---
+++ web/src/wrapper/ProtectedRouteWrapper.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {useAuth} from "@/providers/AuthContext.tsx";
+import {Navigate, useLocation} from "react-router-dom";
+import {Spinner} from "@/components/Spinner.tsx";
+
+const ProtectedRouteWrapper = ({children}: { children: React.ReactNode }) => {
+    const location = useLocation();
+    const {authState, isLoading} = useAuth();
+
+
+    if (isLoading) {
+        return (
+            <div className="flex items-center justify-center min-h-screen">
+                <Spinner/>
+            </div>
+        );
+    }
+
+
+    if (!authState?.token) {
+        return <Navigate to="/login" state={{from: location}}/>
+    }
+
+    return <>{children}</>
+};
+
+export default ProtectedRouteWrapper;
