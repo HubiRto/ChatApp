229dfb6c8e5a2cbcc2ef61d0216c87740aa535de
hubirto <hubert.rybicki.hr@gmail.com>
2024-07-16 19:13:46 +0200

    Add new routes in main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -11,6 +11,9 @@ import HomePage from "@/pages/HomePage.tsx";
 import LoginPage from "@/pages/auth/LoginPage.tsx";
 import RegisterPage from "@/pages/auth/RegisterPage.tsx";
 import ErrorPage from "@/pages/ErrorPage.tsx";
+import ConversationsPage from "@/pages/ConversationsPage.tsx";
+import ConversationPage from "@/pages/ConversationPage.tsx";
+import FriendsPage from "@/pages/FriendsPage.tsx";
 
 
 const wrapWithProtectedRoute = (element: React.ReactNode) => <ProtectedRouteWrapper children={element}/>;
@@ -20,6 +23,24 @@ const sidebarWrapper = (element: React.ReactNode) => <SidebarWrapper children={e
 
 const routesConfig = [
     {path: "/", element: <HomePage/>, wrappers: [wrapWithProtectedRoute, sidebarWrapper], errorElement: <ErrorPage/>},
+    {
+        path: "/conversations",
+        element: <ConversationsPage/>,
+        wrappers: [wrapWithProtectedRoute, sidebarWrapper],
+        errorElement: <ErrorPage/>
+    },
+    {
+        path: "/conversations/:conversationId",
+        element: <ConversationPage/>,
+        wrappers: [wrapWithProtectedRoute, sidebarWrapper],
+        errorElement: <ErrorPage/>
+    },
+    {
+        path: "/friends",
+        element: <FriendsPage/>,
+        wrappers: [wrapWithProtectedRoute, sidebarWrapper],
+        errorElement: <ErrorPage/>
+    },
     {path: "/login", element: <LoginPage/>, wrappers: [isLoginWrapper], errorElement: <ErrorPage/>},
     {path: "/register", element: <RegisterPage/>, wrappers: [isLoginWrapper], errorElement: <ErrorPage/>},
 ];
